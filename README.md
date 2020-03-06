287c65003b6f353c24548a664ac84aa8ca0a5c25
Marshall Dugan <marshall.dugan@gmail.com>
2020-03-06 18:59:25 -0500

    final code

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Note-Taker
+# Node-Taker
+  [![GitHub license](https://img.shields.io/badge/license-MIT-blue)](https://github.com/marshdugan)
+
+# Description
+
+This program allows the user to type and delete notes. This program used express to run on a server.
+
+# Table of Contents
+
+  * [Installation](#Installation)
+
+  * [Usage](#Usage)
+
+  * [License](#License)
+
+  * [Contributing](#Contributing)
+
+  * [Tests](#Tests)
+  
+  * [Questions](#Questions)
+
+# Installation
+  To install necessary dependencies, run the following command:
+  ~~~
+  npm i
+  ~~~
+
+# Usage
+  The html, js, and css files are located in the public folder while the notes are written to the db folder. Run server.js to start the program.
+
+# License
+  This project is licensed under the MIT license.
+
+# Contributing
+  If you would like to contribute start an issure on GitHub.
+
+# Tests
+  To run tests, run the following command:
+  ~~~
+  npm test
+  ~~~
+
+# Questions
+  <img src="https://avatars2.githubusercontent.com/u/54903099?v=4" alt="avatar" style="border-radius: 16px" width="30"/>
+
+  If you have any questions about the repo, open an issue or contact [marshdugan](https://api.github.com/users/marshdugan) or send me an email at null;
+
--- server.js
+++ server.js
@@ -1,1 +1,66 @@
-//
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+let db = require("./db/db.json");
+
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.json(db);
+});
+
+app.post("/api/notes", function(req, res) {
+    const newNote = req.body;
+    newNote.id = db.length - 1;
+
+    db.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(db), error => {
+        if (error) throw error;
+    });
+    res.json(newNote);
+});
+
+
+app.delete("/api/notes/:id", function(req, res) {
+    resetIds();
+    const chosen = parseInt(req.params.id);
+    for (let i = 0; i < db.length; i++) {
+        if (chosen === db[i].id) {
+            db.splice(chosen, 1)
+            fs.writeFile("./db/db.json", JSON.stringify(db), error => {
+                if (error) throw error;
+            });
+            return res.json(db);
+        }
+    }
+    return res.json(false);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+function resetIds() {
+    for (let i = 0; i < db.length; i++) {
+        db[i].id = i;
+    }
+}
+
+
+
+//Listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
